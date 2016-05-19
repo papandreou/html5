689d25e7b37fd69de396afc08f1bdce43c5a4ffe
Maarten Winter <mw@one.com>
2016-05-19 12:59:58 +0200

    Add failing test for attribute doubling when chunking and html entities are involved

--- test/functional/streaming-test.js
+++ test/functional/streaming-test.js
@@ -17,3 +17,36 @@ test('Streamed data', function(t) {
     em.emit('data', 'm>emergency</em> broadcast system');
     em.emit('end');
 })
+
+test('Streamed data, chunked up attribute value with entity', function(t) {
+    t.plan(1)
+    var em = new events.EventEmitter();
+    var p = new HTML5.Parser();
+    p.on('end', function() {
+        t.equal(
+            serialize(p.document),
+            '<html>\n' +
+            '  <head>\n' +
+            '  <body>\n' +
+            '    <p>\n' +
+            '      "This is a test of the "\n' +
+            '      <em>\n' +
+            '        <a>\n' +
+            '          href="htt&p://foo.bar/baz/quux.html"\n' +
+            '          "emergency"\n' +
+            '      " broadcast system"\n'
+        );
+        t.end()
+    })
+    p.parse(em);
+    [
+        '<p>This is a',
+        ' test of the <em><a href="htt&amp;',
+        'p://foo.bar/baz/quux.html">emergency</a>',
+        '</em> broadcast system'
+    ].forEach(function (chunk) {
+        em.emit('data', chunk);
+    });
+
+    em.emit('end');
+})
